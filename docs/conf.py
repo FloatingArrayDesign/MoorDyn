08237d7096a3e16d0302876fd9a89c4386418357
Matt Hall <5151457+mattEhall@users.noreply.github.com>
2021-02-15 01:03:13 -0700

    Edited Sphinx conf.py file to support DECLDIR attribute.

--- docs/conf.py
+++ docs/conf.py
@@ -80,3 +80,7 @@ html_theme = 'sphinx_rtd_theme' # need to run pip install sphinx_rtd_theme to us
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
+
+
+cpp_id_attributes = ['DECLDIR']
+
