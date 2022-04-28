31f328adbd10b925b12ab1a0a7a06c74e1cc45c0
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2022-04-28 09:27:10 +0200

    Upgrade the DLL cross-compiling (batch files has no options to optimize)

--- compile/DLL/compile32.sh
+++ compile/DLL/compile32.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-CXX=i686-w32-mingw32-g++ make clean
-CXX=i686-w32-mingw32-g++ make
+COPTS=''
+
+for arg in "$@"; do
+    if [ $arg = "--optimize" ]; then
+        COPTS='-msse2 -ffast-math -DMOORDYN_SINGLEPRECISSION'
+    fi
+done
+
+CXX=i686-w32-mingw32-g++ CC=i686-w32-mingw32-gcc make clean
+CXX=i686-w32-mingw32-g++ CC=i686-w32-mingw32-gcc make COPTS="$COPTS" -j
