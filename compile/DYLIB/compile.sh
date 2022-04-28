a7e01a59cf26cd273fdb67b2fb0c9b4ce8985edd
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2022-04-28 09:19:31 +0200

    Easy option to optimize when cross compiling

--- compile/DYLIB/compile.sh
+++ compile/DYLIB/compile.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+COPTS=''
+
+for arg in "$@"; do
+    if [ $arg = "--optimize" ]; then
+        COPTS='-msse2 -ffast-math -DMOORDYN_SINGLEPRECISSION'
+    fi
+done
+
 CXX=clang++ CC=clang make clean
-CXX=clang++ CC=clang make
+CXX=clang++ CC=clang make COPTS="$COPTS" -j
