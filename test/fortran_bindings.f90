24c34f93db3d6ab8d893b28a5f95ab5c6f0e2348
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2022-07-28 06:49:00 +0200

    Basic Fortran functionality

--- test/fortran_bindings.f90
+++ test/fortran_bindings.f90
@@ -1,6 +1,6 @@
 program main
     use, intrinsic :: iso_fortran_env, only: real64
-    use, intrinsic :: iso_c_binding, only: c_ptr
+    use, intrinsic :: iso_c_binding, only: c_ptr, c_associated
     use moordyn
 
     character(len=28) :: infile
@@ -8,10 +8,23 @@ program main
     real(real64), allocatable, target :: xd(:)
     real(real64), allocatable, target :: f(:)
     real(real64) :: t, dt
-    integer :: err
+    integer :: err, n_dof
     type(c_ptr) :: system
 
     infile = 'Mooring/lines.txt'
+
+    system = MD_Create(infile)
+    if ( .not.c_associated(system) ) then
+      stop 1
+    end if
+
+    err = MD_NCoupledDOF( system, n_dof )
+    if ( err /= MD_SUCESS ) then
+      stop 1
+    elseif ( n_dof /= 9 ) then
+      print *,"3x3 = 9 DOFs were expected, not ", n_dof
+    end if
+
     allocate ( x(0:8) )
     allocate ( xd(0:8) )
     allocate ( f(0:8) )
@@ -27,32 +40,28 @@ program main
     x(6) = -2.6
     x(7) = -4.5
     x(8) = -70.0
+    xd = 0.0
+    f = 0.0
 
-    system = MD_Create(infile)
-    err = MD_Close(system)
-    if ( err /= 0 ) then
-      print *,"Failure closing the moordyn simulation: ", err
-      stop 1
-    end if
-
-    err = MD_Init(x, xd, infile)
-    if ( err /= 0 ) then
+    err = MD_Init(system, x, xd)
+    if ( err /= MD_SUCESS ) then
       print *,"Failure during the mooring initialization: ", err
       stop 1
     end if
 
     t = 0
     dt = 0.5
-    err = MD_Step(x, xd, f, t, dt)
-    if ( err /= 0 ) then
+    err = MD_Step(system, x, xd, f, t, dt)
+    if ( err /= MD_SUCESS ) then
       print *,"Failure during the mooring step: ", err
       stop 1
     end if
 
-    err = MD_End()
-    if ( err /= 0 ) then
-      print *,"Failure closing Moordyn: ", err
+    err = MD_Close(system)
+    if ( err /= MD_SUCESS ) then
+      print *,"Failure closing the moordyn simulation: ", err
       stop 1
     end if
+
 
 end program main
--- wrappers/fortran/MoorDyn.f90
+++ wrappers/fortran/MoorDyn.f90
@@ -7,9 +7,28 @@ module moordyn
 
   implicit none
 
+  ! The error codes, see MoorDynAPI.h
+  integer, parameter :: MD_SUCESS = 0
+  integer, parameter :: MD_INVALID_INPUT_FILE = -1
+  integer, parameter :: MD_INVALID_OUTPUT_FILE = -2
+  integer, parameter :: MD_INVALID_INPUT = -3
+  integer, parameter :: MD_NAN_ERROR = -4
+  integer, parameter :: MD_MEM_ERROR = -5
+  integer, parameter :: MD_INVALID_VALUE = -6
+  integer, parameter :: MD_NON_IMPLEMENTED = -7
+  integer, parameter :: MD_UNHANDLED_ERROR = -255
+
+  ! The log levels
+  integer, parameter :: MD_ERR_LEVEL = 3
+  integer, parameter :: MD_WRN_LEVEL = 2
+  integer, parameter :: MD_MSG_LEVEL = 1
+  integer, parameter :: MD_DBG_LEVEL = 0
+  integer, parameter :: MD_NO_OUTPUT = 4096
+
   private
 
-  public :: MD_Create, MD_Close, MD_Init, MD_Step, MD_End
+  public :: MD_Create, MD_NCoupledDOF, MD_SetVerbosity, MD_SetLogFile, &
+            MD_SetLogLevel, MD_Log, MD_Init, MD_Step, MD_Close
 
   interface
 
@@ -19,34 +38,66 @@ module moordyn
       type(c_ptr) :: rc
     end function MoorDyn_Create
 
-    function MoorDyn_Close(instance) bind(c, name='MoorDyn_Close') result(rc)
+    function MoorDyn_NCoupledDOF(instance, n) bind(c, name='MoorDyn_NCoupledDOF') result(rc)
       import :: c_ptr, c_int
       type(c_ptr), value, intent(in) :: instance
+      integer(c_int), intent(out) :: n
       integer(c_int) :: rc
-    end function MoorDyn_Close
+    end function MoorDyn_NCoupledDOF
 
-    function MoorDynInit(x, xd, f) bind(c, name='MoorDynInit') result(rc)
-      import :: c_char, c_ptr, c_int
+    function MoorDyn_SetVerbosity(instance, n) bind(c, name='MoorDyn_SetVerbosity') result(rc)
+      import :: c_ptr, c_int
+      type(c_ptr), value, intent(in) :: instance
+      integer(c_int), intent(out) :: n
+      integer(c_int) :: rc
+    end function MoorDyn_SetVerbosity
+
+    function MoorDyn_SetLogFile(instance, f) bind(c, name='MoorDyn_SetLogFile') result(rc)
+      import :: c_ptr, c_char, c_int
+      type(c_ptr), value, intent(in) :: instance
       character(kind=c_char), intent(in) :: f(*)
+      integer(c_int) :: rc
+    end function MoorDyn_SetLogFile
+
+    function MoorDyn_SetLogLevel(instance, n) bind(c, name='MoorDyn_SetLogLevel') result(rc)
+      import :: c_ptr, c_int
+      type(c_ptr), value, intent(in) :: instance
+      integer(c_int), intent(out) :: n
+      integer(c_int) :: rc
+    end function MoorDyn_SetLogLevel
+
+    function MoorDyn_Log(instance, n, f) bind(c, name='MoorDyn_Log') result(rc)
+      import :: c_ptr, c_int, c_char
+      type(c_ptr), value, intent(in) :: instance
+      integer(c_int), intent(in) :: n
+      character(kind=c_char), intent(in) :: f(*)
+      integer(c_int) :: rc
+    end function MoorDyn_Log
+
+    function MoorDyn_Init(instance, x, xd) bind(c, name='MoorDyn_Init') result(rc)
+      import :: c_ptr, c_int
+      type(c_ptr), value, intent(in) :: instance
       type(c_ptr), value, intent(in) :: x
       type(c_ptr), value, intent(in) :: xd
       integer(c_int) :: rc
-    end function MoorDynInit
+    end function MoorDyn_Init
 
-    function MoorDynStep(x, xd, f, t, dt) bind(c, name='MoorDynStep') result(rc)
+    function MoorDyn_Step(instance, x, xd, f, t, dt) bind(c, name='MoorDyn_Step') result(rc)
       import :: c_ptr, c_double, c_int
+      type(c_ptr), value, intent(in) :: instance
       type(c_ptr), value, intent(in) :: x
       type(c_ptr), value, intent(in) :: xd
       type(c_ptr), value, intent(in) :: f
       real(c_double), intent(inout) :: t
       real(c_double), intent(inout) :: dt
       integer(c_int) :: rc
-    end function MoorDynStep
+    end function MoorDyn_Step
 
-    function MoorDynClose() bind(c, name='MoorDynClose') result(rc)
-      import :: c_int
+    function MoorDyn_Close(instance) bind(c, name='MoorDyn_Close') result(rc)
+      import :: c_ptr, c_int
+      type(c_ptr), value, intent(in) :: instance
       integer(c_int) :: rc
-    end function MoorDynClose
+    end function MoorDyn_Close
 
   end interface
 
@@ -59,33 +110,68 @@ contains
     MD_Create = MoorDyn_Create(trim(f) // c_null_char)
   end function MD_Create
 
-  integer function MD_Close(instance)
+  integer function MD_NCoupledDOF(instance, n)
     use iso_c_binding
     type(c_ptr), intent(in) :: instance
-    MD_Close = MoorDyn_Close(instance)
-  end function MD_Close
+    integer(c_int), intent(out) :: n
+    integer err
+    MD_NCoupledDOF = MoorDyn_NCoupledDOF(instance, n)
+  end function MD_NCoupledDOF
 
-  integer function MD_Init(x, xd, f)
+  integer function MD_SetVerbosity(instance, n)
     use iso_c_binding
+    type(c_ptr), intent(in) :: instance
+    integer(c_int), intent(out) :: n
+    integer err
+    MD_SetVerbosity = MoorDyn_SetVerbosity(instance, n)
+  end function MD_SetVerbosity
+
+  integer function MD_SetLogFile(instance, f)
+    use iso_c_binding
+    type(c_ptr), intent(in) :: instance
+    character(*), intent(in) :: f
+    MD_SetLogFile = MoorDyn_SetLogFile(instance, trim(f) // c_null_char)
+  end function MD_SetLogFile
+
+  integer function MD_SetLogLevel(instance, n)
+    use iso_c_binding
+    type(c_ptr), intent(in) :: instance
+    integer(c_int), intent(out) :: n
+    integer err
+    MD_SetLogLevel = MoorDyn_SetLogLevel(instance, n)
+  end function MD_SetLogLevel
+
+  integer function MD_Log(instance, n, f)
+    use iso_c_binding
+    type(c_ptr), intent(in) :: instance
+    integer(c_int), intent(in) :: n
+    character(*), intent(in) :: f
+    MD_Log = MoorDyn_Log(instance, n, trim(f) // c_null_char)
+  end function MD_Log
+
+  integer function MD_Init(instance, x, xd)
+    use iso_c_binding
+    type(c_ptr), intent(in) :: instance
     real(c_double), intent(in), target :: x(:)
     real(c_double), intent(in), target :: xd(:)
-    character(*), intent(in) :: f
-    MD_Init = MoorDynInit(c_loc(x), c_loc(xd), trim(f) // c_null_char)
+    MD_Init = MoorDyn_Init(instance, c_loc(x), c_loc(xd))
   end function MD_Init
 
-  integer function MD_Step(x, xd, f, t, dt)
+  integer function MD_Step(instance, x, xd, f, t, dt)
     use iso_c_binding
+    type(c_ptr), intent(in) :: instance
     real(c_double), intent(in), target :: x(:)
     real(c_double), intent(in), target :: xd(:)
     real(c_double), intent(in), target :: f(:)
     real(c_double), intent(inout) :: t
     real(c_double), intent(inout) :: dt
-    MD_Step = MoorDynStep(c_loc(x), c_loc(xd), c_loc(f), t, dt)
+    MD_Step = MoorDyn_Step(instance, c_loc(x), c_loc(xd), c_loc(f), t, dt)
   end function MD_Step
 
-  integer function MD_End()
+  integer function MD_Close(instance)
     use iso_c_binding
-    MD_End = MoorDynClose()
-  end function MD_End
+    type(c_ptr), intent(in) :: instance
+    MD_Close = MoorDyn_Close(instance)
+  end function MD_Close
 
 end module moordyn
