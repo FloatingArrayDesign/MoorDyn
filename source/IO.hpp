16082f78d8006dc121d6920bdd990b95182cdca7
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2022-07-30 08:34:21 +0200

    Copyright notes

--- source/IO.hpp
+++ source/IO.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Matt Hall
+ * Copyright (c) 2022, Jose Luis Cercos-Pita <jlc@core-marine.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
