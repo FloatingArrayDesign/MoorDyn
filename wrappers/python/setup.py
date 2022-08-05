89b87296972a82cfe8fdafa7fc3571d5fc9e83d1
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2022-08-05 18:01:45 +0200

    cibuildwheel

---
+++ wrappers/python/setup.py
@@ -0,0 +1,71 @@
+"""
+Copyright (c) 2022, Jose Luis Cercos-Pita <jlc@core-marine.com>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+# This file is used to build self-contained Python wrappers, which does not
+# depends on the prebuilt libmoordyn.so.2. This is ideal to be used within
+# cibuildwheel, which is producing the artifacts that can be uploaded later to
+# Pipy
+# In general you do not want to use this script by your own. It is meant for the
+# core developers to setup the pipy packages
+
+import os
+from setuptools import setup, find_packages, Extension
+import sysconfig
+
+
+MOORDYN_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            '..', '..', 'source')
+MOORDYN_SRCS = []
+for f in os.listdir(MOORDYN_PATH):
+    if not os.path.isfile(os.path.join(MOORDYN_PATH, f)):
+        continue
+    if not f.lower().endswith(".c") and not f.lower().endswith(".cpp"):
+        continue
+    MOORDYN_SRCS.append(os.path.join('..', '..', 'source', f))
+
+
+cmoordyn = Extension('cmoordyn',
+                     sources=MOORDYN_SRCS + ['cmoordyn.cpp'],
+                     language='c++',
+                     include_dirs=[MOORDYN_PATH, ],
+                     define_macros=[('MoorDyn_EXPORTS', 'None')],
+                     )
+
+setup(
+    name='moordyn',
+    version='2.0.0',
+    description='MoorDyn for Python',
+    author='Jose Luis Cercos-Pita',
+    author_email='jlc@core-marine.com',
+    ext_modules = [cmoordyn],
+    packages=find_packages(include=['moordyn', 'moordyn.*']),
+    install_requires=[],
+    setup_requires=['cython', 'pytest-runner'],
+    tests_require=['pytest'],
+)
---
+++ wrappers/python/tests/test_minimal.py
@@ -0,0 +1,68 @@
+from unittest import TestCase, main as unittest_main
+import os
+import tempfile
+import moordyn
+
+
+def setup_case(filename):
+    """Setup a case in a temporal folder
+
+    Parameters:
+    filename (str): The filename in the "Example" subfolder
+
+    Returns:
+    str: The folder where the case has been generated
+    """
+    tmp_folder = tempfile.mkdtemp()
+    # The input should be in the Mooring/lines.txt subfolder.
+    # See MoorDyn.cpp:287
+    input_folder = os.path.join(tmp_folder, "Mooring")
+    os.mkdir(input_folder)
+    r_path = os.path.join(
+        os.path.dirname(__file__), "Example", filename)
+    w_path = os.path.join(input_folder, "lines.txt")
+    with open(r_path, "r") as r_file, open(w_path, "w") as w_file:
+        w_file.write(r_file.read())
+    return tmp_folder
+
+
+
+class MinimalTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_no_input(self):
+        try:
+            system = moordyn.Create()
+        except RuntimeError:
+            return
+        self.assertEqual(system, None, "The creation should fail")
+
+    def test_one_iter(self):
+        tmp_folder = setup_case("lines.txt")
+        cwd = os.getcwd()
+        os.chdir(tmp_folder)
+        system = moordyn.Create()
+        x = []
+        for i in range(4, 7):
+            conn = moordyn.GetConnection(system, i)
+            x = x + list(moordyn.GetConnectPos(conn))
+        v = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.assertEqual(moordyn.Init(system, x, v), 0,
+                         "Failure initializing the lines")
+        self.assertEqual(moordyn.NCoupledDOF(system), 9,
+                         "Wrong number of coupled DOFs")
+        self.assertEqual(moordyn.GetNumberLines(system), 3)
+        os.chdir(cwd)
+        v[0] = 0.1
+        forces = moordyn.Step(system, x, v, 0.0, 0.5)
+        print(forces)
+        self.assertEqual(moordyn.Close(system),
+                         0, "Failure finishing MoorDyn")
+        f_ref = 7.2e5
+        self.assertTrue(abs(forces[0] - f_ref) / f_ref < 0.01,
+                        "Wrong surge force")
+
+
+if __name__ == '__main__':
+    unittest_main()
