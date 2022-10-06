a56ccfc9655ffa88698e9736c91af48571b221ab
djanders19 <davidias.j.a@gmail.com>
2022-10-06 18:25:50 -0400

    revert DLL changes for compiling in local environment

--- compile/DLL/compile64.bat
+++ compile/DLL/compile64.bat
@@ -3,7 +3,7 @@ echo on
 rem  temporarily add 64 bit compiler directory to path
 set PATH=C:\mingw64\bin;%PATH%
 
-minGW32-make clean
+rem minGW32-make clean
 
-minGW32-make all
+minGW32-make
 pause
--- compile/DLL/compile64_debug.bat
+++
@@ -1,9 +0,0 @@
-echo on
-
-rem  temporarily add 64 bit compiler directory to path
-set PATH=C:\mingw64\bin;%PATH%
-
-rem minGW32-make clean
-
-minGW32-make debug
-@REM pause
