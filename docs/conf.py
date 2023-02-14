883ef8041d367e4502f0ce700466daadce457120
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2023-02-14 13:59:07 +0100

    Increased the version

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ copyright = '2021, National Renewable Energy Laboratory'
 author = 'Matt Hall'
 
 # The full version, including alpha/beta/rc tags
-release = '1.01.01'
+release = '2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
