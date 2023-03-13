86b13211f4950cefd6c96e56520eff547353b297
19danderson <anderson.davi@northeastern.edu>
2023-03-13 10:35:02 -0400

    seafloor test files

---
+++ tests/Mooring/3D_seafloor/make_3d_seafloor.py
@@ -0,0 +1,25 @@
+import numpy as np
+BASE_DEPTH = -30
+x = np.linspace(-50.0, 50.0, num=100)
+y = np.linspace(-2.0, 2.0, num=5)
+
+# Define 2d grid for seafloor:
+xx, yy = np.meshgrid(x,y)
+
+with open('seafloor_profile_3d.txt', 'w') as f:
+    # First line is number of (x,y) coords
+    f.write(f"{len(x)} {len(y)}\n")
+    
+    # Next two lines specify the axis (tick) values for x, y respectively
+    for xx in x:
+        f.write(f"{xx} ")
+    f.write("\n")
+
+    for yy in y:
+        f.write(f"{yy} ")
+    f.write("\n")
+
+    # All the remaining lines specify an (x, y, z) coordinate
+    for idx, xs in enumerate(x):
+        for idy, ys in enumerate(y):
+            f.write(f"{xs} {ys} {BASE_DEPTH + ys}\n")
