a01cc96ffed8607f2da13a36400cad6f5e2268ba
Alex Kinley <a.kinley@kelsonmarine.com>
2023-06-05 16:09:40 -0400

    fix warnings from non-virtual destructors

--- source/Connection.hpp
+++ source/Connection.hpp
@@ -66,7 +66,7 @@ typedef std::shared_ptr<Waves> WavesRef;
  *          weight or float via the point's mass and volume parameters
  *  - Coupled: The connection position and velocity is externally imposed
  */
-class Connection : public io::IO
+class Connection final : public io::IO
 {
   public:
 	/** @brief Costructor
@@ -353,7 +353,7 @@ class Connection : public io::IO
 	 * from the definition file
 	 * @return The packed data
 	 */
-	virtual std::vector<uint64_t> Serialize(void);
+	std::vector<uint64_t> Serialize(void);
 
 	/** @brief Unpack the data to restore the Serialized information
 	 *
@@ -361,7 +361,7 @@ class Connection : public io::IO
 	 * @param data The packed data
 	 * @return A pointer to the end of the file, for debugging purposes
 	 */
-	virtual uint64_t* Deserialize(const uint64_t* data);
+	uint64_t* Deserialize(const uint64_t* data);
 
 #ifdef USE_VTK
 	/** @brief Produce a VTK object
