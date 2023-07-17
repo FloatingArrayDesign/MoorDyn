c5b959ebd2acfedddbcfbefbc7e29fc26f1b0cfb
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2023-07-17 10:43:08 +0200

    Upgraded the rods test to CATCH2

--- tests/rods.cpp
+++ tests/rods.cpp
@@ -47,8 +47,7 @@
 
 using namespace std;
 
-bool
-added_mass()
+TEST_CASE("Added mass")
 {
 	// Do the math for our expected acceleration given the mass and that
 	// Ca = 1.0
@@ -68,54 +67,30 @@ added_mass()
 	cout << endl << " => " << __PRETTY_FUNC_NAME__ << "..." << endl;
 
 	MoorDyn system = MoorDyn_Create("Mooring/rod_tests/AddedMass.txt");
-	if (!system) {
-		cerr << "Failure Creating the Mooring system" << endl;
-		return false;
-	}
+	REQUIRE(system);
 
 	err = MoorDyn_Init(system, NULL, NULL);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure during the mooring initialization: " << err << endl;
-		return false;
-	}
+	REQUIRE(err == MOORDYN_SUCCESS);
 
 	double t = 0, Tmax = 2.5, dt = 0.1;
 
 	while (t < Tmax) {
 		err = MoorDyn_Step(system, NULL, NULL, NULL, &t, &dt);
-		if (err != MOORDYN_SUCCESS) {
-			cerr << "Failure during the mooring initialization: " << err
-			     << endl;
-			return false;
-		}
+		REQUIRE(err == MOORDYN_SUCCESS);
 
 		const auto rod = MoorDyn_GetRod(system, 1);
 
 		unsigned int n_nodes;
 		err = MoorDyn_GetRodN(rod, &n_nodes);
-		if (err != MOORDYN_SUCCESS) {
-			cerr << "Failure getting the number of nodes for rod " << 1 << ": "
-			     << err << endl;
-			return false;
-		}
+		REQUIRE(err == MOORDYN_SUCCESS);
 
 		double expected_z = -10 + 0.5 * acceleration * t * t;
 		double pos[3];
 		for (unsigned int i = 0; i < n_nodes; i++) {
 
 			err = MoorDyn_GetRodNodePos(rod, i, pos);
-			if (err != MOORDYN_SUCCESS) {
-				cerr << "Failure getting the position of nodes " << i
-				     << " for rod 1"
-				     << ": " << err << endl;
-				return false;
-			}
-			if (!isclose(pos[2], expected_z, 1e-8, 1e-10)) {
-				cerr << "Node " << i << " of Rod 1 should have a z position of "
-				     << expected_z << " but has a z pos of " << pos[2]
-				     << " at t=" << t << endl;
-				return false;
-			}
+			REQUIRE(err == MOORDYN_SUCCESS);
+			REQUIRE(isclose(pos[2], expected_z, 1e-8, 1e-10));
 		}
 		// when the rod get higher than z = -1, stop simulating
 		if (expected_z > -1.0) {
@@ -124,12 +99,7 @@ added_mass()
 	}
 
 	err = MoorDyn_Close(system);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure closing Moordyn: " << err << endl;
-		return false;
-	}
-
-	return true;
+	REQUIRE(err == MOORDYN_SUCCESS);
 }
 
 /** @brief Pinned rod horizontally lying, which should float towards the
@@ -138,167 +108,74 @@ added_mass()
  * Since there is no damping forces, the rods will be oscillating from one side
  * to the other
  */
-bool
-pinned_floating()
+TEST_CASE("Pinned floating rod")
 {
 	int err;
-	cout << endl << " => " << __PRETTY_FUNC_NAME__ << "..." << endl;
-
 	MoorDyn system = MoorDyn_Create("Mooring/RodPinnedFloating.txt");
-	if (!system) {
-		cerr << "Failure Creating the Mooring system" << endl;
-		return false;
-	}
+	REQUIRE(system);
 
 	err = MoorDyn_Init(system, NULL, NULL);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure during the mooring initialization: " << err << endl;
-		return false;
-	}
+	REQUIRE(err == MOORDYN_SUCCESS);
 
 	double t = 0, T = 10.0;
 	err = MoorDyn_Step(system, NULL, NULL, NULL, &t, &T);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure during the mooring initialization: " << err << endl;
-		return false;
-	}
+	REQUIRE(err == MOORDYN_SUCCESS);
 
 	unsigned int n_rods;
 	err = MoorDyn_GetNumberRods(system, &n_rods);
 	for (unsigned int i_rod = 1; i_rod <= n_rods; i_rod++) {
 		const auto rod = MoorDyn_GetRod(system, i_rod);
-		if (!rod) {
-			cerr << "Failure getting the rod " << i_rod << endl;
-			return false;
-		}
+		REQUIRE(rod);
 
 		unsigned int n_nodes;
 		err = MoorDyn_GetRodN(rod, &n_nodes);
-		if (err != MOORDYN_SUCCESS) {
-			cerr << "Failure getting the number of nodes for rod " << i_rod
-			     << ": " << err << endl;
-			return false;
-		}
+		REQUIRE(err == MOORDYN_SUCCESS);
 		// Check that the rod is floating upwards
 		double pos[3];
 		err = MoorDyn_GetRodNodePos(rod, 0, pos);
-		if (err != MOORDYN_SUCCESS) {
-			cerr << "Failure getting first node position for rod " << i_rod
-			     << ": " << err << endl;
-			return false;
-		}
-		cout << pos[0] << ", " << pos[1] << ", " << pos[2] << endl;
+		REQUIRE(err == MOORDYN_SUCCESS);
 		const double z0 = pos[2];
 		err = MoorDyn_GetRodNodePos(rod, n_nodes, pos);
-		if (err != MOORDYN_SUCCESS) {
-			cerr << "Failure getting last node position for rod " << i_rod
-			     << ": " << err << endl;
-			return false;
-		}
+		REQUIRE(err == MOORDYN_SUCCESS);
 		const double z1 = pos[2];
-		cout << pos[0] << ", " << pos[1] << ", " << pos[2] << endl;
-
-		if (z1 < z0) {
-			cerr << "The last node is below the first one for rod " << i_rod
-			     << ": " << z1 << " vs. " << z0 << endl;
-			return false;
-		}
+		REQUIRE(z1 >= z0);
 	}
 
 	err = MoorDyn_Close(system);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure closing Moordyn: " << err << endl;
-		return false;
-	}
-
-	return true;
+	REQUIRE(err == MOORDYN_SUCCESS);
 }
 
-/** @brief Rod hanging from 2 identical ropes, which should move horizontally
- */
-bool
-hanging()
+TEST_CASE("Hanging rod")
 {
 	int err;
-	cout << endl << " => " << __PRETTY_FUNC_NAME__ << "..." << endl;
-
 	MoorDyn system = MoorDyn_Create("Mooring/RodHanging.txt");
-	if (!system) {
-		cerr << "Failure Creating the Mooring system" << endl;
-		return false;
-	}
+	REQUIRE(system);
 
 	err = MoorDyn_Init(system, NULL, NULL);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure during the mooring initialization: " << err << endl;
-		return false;
-	}
+	REQUIRE(err == MOORDYN_SUCCESS);
 
 	double t = 0, T = 10.0;
 	err = MoorDyn_Step(system, NULL, NULL, NULL, &t, &T);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure during the mooring initialization: " << err << endl;
-		return false;
-	}
+	REQUIRE(err == MOORDYN_SUCCESS);
 
 	const auto rod = MoorDyn_GetRod(system, 1);
-	if (!rod) {
-		cerr << "Failure getting the rod" << endl;
-		return false;
-	}
+	REQUIRE(rod);
 
 	unsigned int n_nodes;
 	err = MoorDyn_GetRodN(rod, &n_nodes);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure getting the number of nodes: " << err << endl;
-		return false;
-	}
+	REQUIRE(err == MOORDYN_SUCCESS);
+
 	// Check that the rod is is not rotating
 	const double l = 20.0;
 	double posa[3], posb[3];
 	err = MoorDyn_GetRodNodePos(rod, 0, posa);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure getting first node position: " << err << endl;
-		return false;
-	}
-	cout << posa[0] << ", " << posa[1] << ", " << posa[2] << endl;
+	REQUIRE(err == MOORDYN_SUCCESS);
 	err = MoorDyn_GetRodNodePos(rod, n_nodes, posb);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure getting last node position: " << err << endl;
-		return false;
-	}
-	cout << posb[0] << ", " << posb[1] << ", " << posb[2] << endl;
+	REQUIRE(err == MOORDYN_SUCCESS);
 
-	if (((posa[0] + 0.5 * l) / l > TOL) || ((posb[0] - 0.5 * l) / l > TOL) ||
-	    (fabs(posa[1]) / l > TOL) || (fabs(posb[1]) / l > TOL) ||
-	    (fabs(posa[2] - posb[2]) / l > TOL)) {
-		cerr << "The rod is rotating" << endl;
-		return false;
-	}
+	REQUIRE((posa[0] + 0.5 * l) / l < TOL);
+	REQUIRE((posb[0] - 0.5 * l) / l < TOL);
 
 	err = MoorDyn_Close(system);
-	if (err != MOORDYN_SUCCESS) {
-		cerr << "Failure closing Moordyn: " << err << endl;
-		return false;
-	}
-
-	return true;
-}
-
-/** @brief Runs all the test
- * @return 0 if the tests have ran just fine. The index of the failing test
- * otherwise
- */
-int
-main(int, char**)
-{
-	if (!pinned_floating())
-		return 1;
-	if (!hanging())
-		return 2;
-	if (!added_mass())
-		return 3;
-
-	cout << "rods.cpp passed successfully" << endl;
-	return 0;
+	REQUIRE(err == MOORDYN_SUCCESS);
 }
