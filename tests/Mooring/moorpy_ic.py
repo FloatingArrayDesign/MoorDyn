d51c21cfe197146df02b97cc8b0a1bf6599006ab
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2024-08-16 09:04:35 +0200

    fix(python): Autonomous MoorPy IC exporter

--- tests/Mooring/moorpy_ic.py
+++ tests/Mooring/moorpy_ic.py
@@ -1,5 +1,9 @@
+import os
+ROOT = os.path.dirname(os.path.realpath(__file__))
+import sys
+sys.path.append(os.path.join(ROOT, '../../wrappers/python/moordyn/'))
+from moorpyic import *
 import argparse
-import moorpy
 
 
 parser = argparse.ArgumentParser(
@@ -9,11 +13,19 @@ parser.add_argument('filename')
 parser.add_argument('-t', '--tol', type=float, default=0.05,
                     help='Equilibrium solver absolute tolerance')
 
+with open(os.path.join(ROOT, '../../CMakeLists.txt'), 'r') as f:
+    txt = f.read()
+    def read_version_line(txt, name):
+        subtxt = "set(" + name + " "
+        txt = txt[txt.find(subtxt) + len(subtxt):]
+        txt = txt[:txt.find(')\n')]
+        return int(txt.strip())
+    version_major = read_version_line(txt, "MOORDYN_MAJOR_VERSION")
+    version_minor = read_version_line(txt, "MOORDYN_MINOR_VERSION")
+
 args = parser.parse_args()
-system = moorpy.System(file=args.filename)
-system.initialize()
-if system.solveEquilibrium(tol=args.tol, display=1):
-    print(f"Saving {args.filename + '.ic'}...")
-    system.export_state(args.filename + '.ic')
-else:
+if not moorpy_ic(args.filename,
+                 tol=args.tol,
+                 version_major=version_major,
+                 version_minor=version_minor):
     print("The system did not converge!")
